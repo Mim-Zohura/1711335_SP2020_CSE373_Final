9f1cf043c20a392e49d547505d1dc4059e8f9ad5
Mim-Zohura <fatima.zohura@northsouth.edu>
2020-06-03 23:42:41 +0600

    main class

--- Final/Question 04/SP2020_CSE373_Final_Q4_1711335.java
+++ Final/Question 04/SP2020_CSE373_Final_Q4_1711335.java
@@ -4,5 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class SP2020_CSE373_Final_Q4_1711335 {
-          
+	public static void main(String args[]) {
+		 int n;
+	      int e;
+	      int s;
+	      Dfs g = null;
+	}
 }
