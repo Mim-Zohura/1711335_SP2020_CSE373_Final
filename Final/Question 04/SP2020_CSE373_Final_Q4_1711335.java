7e81cca0930eedead2c0280e3eebb482951b99a6
Mim-Zohura <fatima.zohura@northsouth.edu>
2020-06-03 23:57:54 +0600

    Dfs

--- Final/Question 04/SP2020_CSE373_Final_Q4_1711335.java
+++ Final/Question 04/SP2020_CSE373_Final_Q4_1711335.java
@@ -9,6 +9,20 @@ public class SP2020_CSE373_Final_Q4_1711335 {
 	      int e;
 	      int s;
 	      Dfs g = null;
+	      private int V;   // No. of vertices 
+
+		    // Array  of lists for Adjacency List Representation 
+		    private LinkedList<Integer> adj[];
+		    
+		 // Constructor 
+		Dfs(int v) {
+	        V = v;
+	        adj = new LinkedList[v];
+	        for (int i = 0; i < v; ++i) {
+	            adj[i] = new LinkedList();
+	        }
+	    }
+		
 	      
 	      
 	}
