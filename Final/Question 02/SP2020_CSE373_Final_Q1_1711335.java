4a8a45054818ea4bb5aa12f0ad35fd6c7a20af79
Mim-Zohura <fatima.zohura@northsouth.edu>
2020-06-03 18:25:08 +0600

    Constructor

--- Final/Question 02/SP2020_CSE373_Final_Q1_1711335.java
+++ Final/Question 02/SP2020_CSE373_Final_Q1_1711335.java
@@ -9,67 +9,20 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
-class Vertex {
-	  public char label;
-	  public boolean Visited;
-	  
-	  public Vertex(char lab) {
-	    label = lab;
-	    Visited = false;
-	  }
-	}
-class Graph{
-	  private final int MAX_VERTS;
-	  private Vertex vertexList[];
-	  private int adjMat[][];
-	  private int nVerts;
-	  private Stack<Integer> s;
-	  public Graph() {
-		    vertexList = new Vertex[MAX_VERTS];
-		    adjMat = new int[MAX_VERTS][MAX_VERTS];
-		    nVerts = 0;
-		    s = new Stack<Integer>();
-		  }
-	  public void addVertex(char lab) {
-		    vertexList[nVerts++] = new Vertex(lab);
-		  }
-	  public void addEdge(int start, int end) {
-		    adjMat[start][end] = 1;
-		    adjMat[end][start] = 1;
-		  }
-	  public void displayVertex(int v) {
-		    System.out.print(vertexList[v].label + " ");
-		  }
-	  public int getAdjUnvisitedVertex (int v) {
-		    for(int j = 0; j < nVerts; j++) {
-		      if(adjMat[v][j] == 1 && vertexList[j].Visited == false) {
-		        return j;
-		      }
-		    }
-		    return -1;
-		  }
-	  public void SP2020_CSE373_Final_Q1_1711335() {
-		 
-			    vertexList[0].Visited = true;
-			    displayVertex(0);
-			    s.push(0);
-			    
-			    while(!s.isEmpty()) {    
-			      int v = getAdjUnvisitedVertex(s.peek());
-			      
-			      if(v == -1) {
-			        s.pop();
-			      } else {
-			        vertexList[v].Visited = true;
-			        displayVertex(v);
-			        s.push(v);
-			      }
-			    }
-			  
-		  }
-}
+
 public class SP2020_CSE373_Final_Q1_1711335 {
-	
+	 private int V;   // No. of vertices 
+
+	    // Array  of lists for Adjacency List Representation 
+	    private LinkedList<Integer> adj[];
+	 // Constructor 
+	SP2020_CSE373_Final_Q1_1711335(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
 	 public static void main(String args[]) {
 	  int n;
       int e;
